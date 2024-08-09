1b86b06cdb7b8020b3813064ed03e8ab533559af
Responsive-web <saharsaeed8090@gmail.com>
2024-08-09 01:55:09 -0700

    Java-script ch#3

---
+++ script.js
@@ -0,0 +1,24 @@
+// Question 1: 
+var age = 14;
+ alert("I am" + " " + age + " " + "Years old.");
+ console.log("I am" + " " + age + " " + "Years old.");
+
+// Question 2:
+ var visit_Site;
+ visit_Site = 14;
+ alert("You have visited this site" + " " + visit_Site + " " + "times");
+ console.log("You have visited this site" + " " + visit_Site + " " + "times");
+
+// Question 3:
+ var birthYear = 2006;
+ document.write("My Birth year is" +" "+ birthYear + "<br/>");
+ console.log("My Birth year is" +" "+ birthYear + "<br/>");
+ document.write("Data type of my declared variable is number.");
+ console.log("Data type of my declared variable is number.");
+
+// Question 4: 
+ var visiterName = "John Doe";
+ var productTitle = "T-shirt"
+ var productQuntity = "5"
+ document.write(visiterName + " " + "ordered" + " " + productQuntity + " " + productTitle + "(s)" +  " " + "on XYZ Clothing store.");
+ console.log(visiterName + " " + "ordered" + " " + productQuntity + " " + productTitle + "(s)" +  " " + "on XYZ Clothing store.");
